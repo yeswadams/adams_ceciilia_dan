a7b54bf258dcc2fc4180c3f0b4eac55d9f9f4511
yeswadams <yeswaadams73@gmail.com>
2026-03-23 17:41:40 +0300

    fixed errors in the code

--- task.js
+++ task.js
@@ -1,72 +1,28 @@
-// function processStudentsData(students) {
-//   let totalScore = 0;
-//   let passedStudents = [];
-//   let failedStudents = [];
-//   let report = "";
-
-//   for (let i = 0; i < students.length; i++) {
-//     let student = students[i]; // Access the student object
-
-//     let name = student.name; // Accessing the students name within the object
-//     let marks = student.marks; // Accessing the students marks within the object
-
-//     totalScore += marks; // totalScore = totalScore + marks
-
-//     if (marks >= 50) {
-//       passedStudents.push(name); // .push() adds an element to the end of the passedStudents array
-//     } else {
-//       failedStudents.push(name); // .push() adds an element to the end of the failedStudents array
-//     }
-
-//     report += name + " scored " + marks + "\n"; // report = report + name + " scored " + marks + "\n"
-//   }
-
-//   let average = totalScore / students.length; // total / no. of total students
-
-//   report += "\nAverage Score: " + average + "\n"; // report = report + "Average Score: " + average + "\n"
-//   report += "Passed: " + passedStudents.join(", ") + "\n"; // report = report + "Passed: " + passedStudents.join(", ") + "\n"
-//   // passedStudents = ["Alice", "Bob", "Charlie"]  => "Alice, Bob, Charlie"
-//   report += "Failed: " + failedStudents.join(", ") + "\n";
-
-//   console.log(report);
-
-//   return {
-//     average: average,
-//     passed: passedStudents,
-//     failed: failedStudents,
-//     report: report,
-//   };
-// }
-
 const students = [
   { name: "Alice", marks: 80 }, // passed
   { name: "Bob", marks: 45 }, // failed
   { name: "Charlie", marks: 60 }, // passed
 ];
 
-// processStudentsData(students);
 
 function processStudentsData(students) {
-  let studentsMarks = getStudentsMarks(students);
-  let average = getAverageMarks(students);
   let report = generateReport(students);
+  let average = getAverageMarks(students);
+  let studentsMarks = getStudentsMarks(students);
   
-  return studentsMarks + "\n" + average + "\n" + report;
+  return report + "\n" + average + "\n" + studentsMarks;
 }
 
 console.log(processStudentsData(students));
 
-// function getStudents(students) {
-//   for (let i = 0; i < students.length; i++) {
-//     let student = students[i];
-//     let marks = student.marks;
-//     let name = student.name; // Use dot notation or bracket notation with a key
-//   }
-//   return students;
-// }
 
-// getStudents(students);
-// console.log(getStudents(students));
+function generateReport(students) {
+    let report = "";
+    students.forEach((student) => {
+       report += student.name + " scored " + student.marks + "\n"
+    })
+    return report
+}
 
 function getStudentsMarks(students) {
   let passedStudents = [];
@@ -85,20 +41,13 @@ function getStudentsMarks(students) {
 // console.log(getStudentsMarks(students));
 // console.log(generateReport(students));
 
+
 function getAverageMarks(students) {
     let totalScore = 0;
     students.forEach((student) => {
         totalScore += student.marks; // totalScore = totalScore + student.marks
     })
     return Avarage = "Avarage Score: " + totalScore / students.length  
-}
-
-function generateReport(students) {
-    let report = "";
-    students.forEach((student) => {
-       report += student.name + " scored " + student.marks + "\n"
-    })
-    return report
 }
 
 
